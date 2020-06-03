5464efca81493e2a0cc62eef59220cbe1f1fa5a3
Todor Garnev <todor.garnev@upnetix.com>
2020-06-03 11:08:32 +0300

    Added brackets component functionality.

--- src/components/Brackets/Brackets.tsx
+++ src/components/Brackets/Brackets.tsx
@@ -1,14 +1,53 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import styles from './Brackets.module.css';
 
 const Brackets: FunctionComponent = () => {
+	const [input, setInput] = useState('');
+	const [message, setMessage] = useState('');
+
+	const checkBrackets = (inputStr: any) => {
+		const openedBracketsList: string[] = [];
+
+		for (let index in inputStr) {
+			const char = inputStr[index];
+
+			if (['(', '[', '{'].includes(char)) {
+				openedBracketsList.push(char);
+			} else {
+				if (![')', ']', '}'].includes(char)) {
+					continue;
+				}
+
+				if (openedBracketsList.length === 0) {
+					setMessage('Incorrect number or order of brackets');
+					return;
+				}
+
+				const lastOpenedBracket = openedBracketsList.pop();
+
+				if ((lastOpenedBracket === '[' && char !== ']') ||
+					(lastOpenedBracket === '(' && char !== ')') ||
+					(lastOpenedBracket === '{' && char !== '}')) {
+						setMessage('Incorrect number or order of brackets');
+				}
+			}
+		}
+
+		if (openedBracketsList.length !== 0) {
+			setMessage('Incorrect number or order of brackets');
+			return;
+		}
+
+		setMessage('Correct order and number of brackets');
+	};
+
 	return (
 		<section className={styles.bracketsContainer}>
 			<div className={styles.inputContainer}>
-				<input className={styles.input} type="text" placeholder="Enter code with brackets" />
-				<div className={styles.message}>Message</div>
+				<input className={styles.input} type="text" placeholder="Enter code with brackets" onChange={(e) => setInput(e.target.value)} />
+				{message && <div className={styles.message}>{message}</div>}
 			</div>
-			<button className={styles.validationButton} type="button">Validate</button>
+			<button className={styles.validationButton} type="button" onClick={() => checkBrackets(input)}>Validate</button>
 		</section>
 	);
 };
